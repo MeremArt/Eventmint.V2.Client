83f53080a5ce01bc6598d7bb8290b0a93df1b15f
Ugo Chinemerem <ugofranklin22@gmail.com>
2024-09-20 15:47:40 +0100

    add:tiplink

--- app/components/AppWalletProvider.tsx
+++ app/components/AppWalletProvider.tsx
@@ -4,6 +4,7 @@ import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
+import { useSearchParams } from "next/navigation";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
@@ -12,7 +13,10 @@ import {
   SolflareWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
 import { BackpackWalletAdapter } from "@solana/wallet-adapter-backpack";
-import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+
+import { TipLinkWalletAdapter } from "@tiplink/wallet-adapter";
+import { TipLinkWalletAutoConnectV2 } from "@tiplink/wallet-adapter-react-ui";
+import { Suspense } from "react";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 export default function AppWalletProvider({
@@ -27,6 +31,11 @@ export default function AppWalletProvider({
       new PhantomWalletAdapter(),
       new SolflareWalletAdapter(),
       new BackpackWalletAdapter(),
+      new TipLinkWalletAdapter({
+        title: "Event mint", // Replace with your Dapp's name
+        clientId: "65783e67-8b6d-4da9-b2ba-40139abc7557", // Replace with your actual client ID
+        theme: "dark", // Choose between "dark", "light", or "system"
+      }),
     ],
     [network]
   );
