b92b3622d870ae3765e57183d9a8d50fe753d9da
Ugo Chinemerem <ugofranklin22@gmail.com>
2024-10-23 08:35:15 +0100

    testing

--- app/regular-user/my-tickets/page.tsx
+++ app/regular-user/my-tickets/page.tsx
@@ -1,23 +1,21 @@
-'use client'
-import React, { useEffect } from 'react'
-import { ticketDummy } from '@/component/ticketResult/ticketDataResult';
-import TicketResult from '@/component/ticketResult';
-import RegularTicket from '@/component/regular-ticket';
-import RegularModal from '@/component/regular-ticket/regular-modal';
-import { useDispatch } from 'react-redux';
-import { updateShowModal } from '@/mainStore/reduxSlices/modalSlice';
-import axios from 'axios';
-const Page: React.FC = () =>  {
-
+"use client";
+import React, { useEffect } from "react";
+import { ticketDummy } from "@/component/ticketResult/ticketDataResult";
+import TicketResult from "@/component/ticketResult";
+import RegularTicket from "@/component/regular-ticket";
+import RegularModal from "@/component/regular-ticket/regular-modal";
+import { useDispatch } from "react-redux";
+import { updateShowModal } from "@/mainStore/reduxSlices/modalSlice";
+import axios from "axios";
+const Page: React.FC = () => {
   const dispatch = useDispatch();
-  const showModal = ()=> {
-    dispatch(updateShowModal())
-}
-
+  const showModal = () => {
+    dispatch(updateShowModal());
+  };
 
   return (
-    <div className='w-full h-full text-white'>
-     <div className="flex items-center content-center gap-6 self-stretch flex-wrap">
+    <div className="w-full h-full text-white">
+      <div className="flex items-center content-center gap-6 self-stretch flex-wrap">
         {ticketDummy.map((item: any, index: any) => (
           <RegularTicket
             key={index}
@@ -34,7 +32,8 @@ const Page: React.FC = () =>  {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
+//test
 
-export default Page
+export default Page;
